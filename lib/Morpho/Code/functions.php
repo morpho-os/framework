f96cc9811cea5f8aa30193fe6e80d6d94d214ed9
morpho-os_team <morpho-os@users.noreply.github.com>
2016-07-13 10:37:52 +0300

    The composerAutoloader() should return an instance, not an array.

--- lib/Morpho/Code/functions.php
+++ lib/Morpho/Code/functions.php
@@ -6,10 +6,10 @@ use Composer\Autoload\ClassLoader;
 /**
  * Returns the first found Composer's autoloader.
  */
-function composerAutoloader(): array {
+function composerAutoloader(): ClassLoader {
     foreach (spl_autoload_functions() as $callback) {
         if (is_array($callback) && $callback[0] instanceof ClassLoader && $callback[1] === 'loadClass') {
-            return $callback;
+            return $callback[0];
         }
     }
     throw new \RuntimeException("Unable to find the Composer's autoloader in the list of autoloaders");
