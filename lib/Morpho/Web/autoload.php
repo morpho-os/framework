239898136a6ac277313b9e15e3b668fffb7310db
morpho-os_team <morpho-os@users.noreply.github.com>
2016-09-26 05:37:06 +0300

    Adjusted some constants.

--- lib/Morpho/Web/autoload.php
+++ lib/Morpho/Web/autoload.php
@@ -1,4 +1,6 @@
 <?php
+// Below are some constants for the web-application-level, see the Morpho/Core/autoload.php for the Core-level constants.
+
 const PUBLIC_DIR_NAME = 'public';
 if (!defined('PUBLIC_DIR_PATH')) {
     define('PUBLIC_DIR_PATH', BASE_DIR_PATH . '/' . PUBLIC_DIR_NAME);
@@ -8,16 +10,15 @@ if (PHP_SAPI !== 'cli' && !chdir(PUBLIC_DIR_PATH)) {
 }
 
 const CSS_DIR_NAME = 'css';
-const DEST_DIR_NAME = 'dest';
+//const DEST_DIR_NAME = 'dest';
 const DOMAIN_DIR_NAME = 'domain';
-const FORM_DIR_NAME = 'form';
+const FONT_DIR_NAME = 'font';
 const IMG_DIR_NAME = 'img';
 const JS_DIR_NAME = 'js';
 const PUBLIC_MODULE_DIR_NAME = 'module';
+const RC_DIR_NAME = 'rc';
 const SCRIPT_DIR_NAME = 'script';
 const SRC_DIR_NAME = 'src';
-const STYL_DIR_NAME = 'styl';
-const TS_DIR_NAME = 'ts';
 const UPLOAD_DIR_NAME = 'upload';
 const VIEW_DIR_NAME = 'view';
 
