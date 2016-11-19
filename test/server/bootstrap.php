f55ab1ed4ef4c7de24abb02f5e1a833f568b709a
morpho-os_team <morpho-os@users.noreply.github.com>
2016-11-19 22:03:37 +0300

    Debug Travis build

--- test/server/bootstrap.php
+++ test/server/bootstrap.php
@@ -1,6 +1,23 @@
 <?php
 date_default_timezone_set('UTC');
 
+if (getenv('TRAVIS')) {
+    (function () {
+        $dsn = 'mysql:dbname=;localhost;charset=UTF8';
+        $db = new \PDO($dsn, 'root', '');
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        $sqls = [
+            'DROP DATABASE IF EXISTS test; CREATE DATABASE test DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci',
+            "GRANT ALL PRIVILEGES ON test.* TO 'root'@'localhost' IDENTIFIED BY ''",
+            "FLUSH PRIVILEGES",
+        ];
+        foreach ($sqls as $sql) {
+            $db->query($sql);
+        }
+    })();
+}
+
 (function () {
     $classLoader = require __DIR__ . '/../../vendor/autoload.php';
     $classLoader->add('MorphoTest', __DIR__ . '/unit');
