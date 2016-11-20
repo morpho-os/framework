43eaa3e9a2506139df3aade2cdf8dc7639743de6
morpho-os_team <morpho-os@users.noreply.github.com>
2016-11-20 08:04:06 +0300

    Removed not used code

--- test/server/bootstrap.php
+++ test/server/bootstrap.php
@@ -1,23 +1,6 @@
 <?php
 date_default_timezone_set('UTC');
 
-/*if (getenv('TRAVIS')) {
-    (function () {
-        $dsn = 'mysql:dbname=;127.0.0.1;charset=UTF8';
-        $db = new \PDO($dsn, 'root', '');
-        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-        $sqls = [
-            'DROP DATABASE IF EXISTS test; CREATE DATABASE test DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci',
-            "GRANT ALL PRIVILEGES ON test.* TO 'root'@'127.0.0.1' IDENTIFIED BY ''",
-            "FLUSH PRIVILEGES",
-        ];
-        foreach ($sqls as $sql) {
-            $db->query($sql);
-        }
-    })();
-}*/
-
 (function () {
     $classLoader = require __DIR__ . '/../../vendor/autoload.php';
     $classLoader->add('MorphoTest', __DIR__ . '/unit');
