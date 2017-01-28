0e8e4b519b1104befe542d614dddc910f046941b
morpho-os_team <morpho-os@users.noreply.github.com>
2017-01-28 14:30:14 +0300

    Added the INDENT constant

--- lib/Morpho/Base/autoload.php
+++ lib/Morpho/Base/autoload.php
@@ -13,6 +13,7 @@ const RESOURCE_TYPE = 'resource';
 
 const TRIM_CHARS = " \t\n\r\x00\x0B";
 const EOL_REGEXP = '~(?>\r\n|\n|\r)~s';
+const INDENT = '    ';
 
 const SHORTEN_TAIL = '...';
 const SHORTEN_LENGTH = 30;
