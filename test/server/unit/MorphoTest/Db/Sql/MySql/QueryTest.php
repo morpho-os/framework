4aadcee05b085e97072720e004929fe764236321
morpho-os_team <morpho-os@users.noreply.github.com>
2017-03-01 08:23:43 +0300

    Added MySql\QueryTest

---
+++ test/server/unit/MorphoTest/Db/Sql/MySql/QueryTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace MorphoTest\Db\Sql\MySql;
+
+use Morpho\Test\TestCase;
+
+class QueryTest extends TestCase {
+    public function testInsert() {
+        $this->markTestIncomplete();
+    }
+}
