0b7790f6fb92eb8d44aebd5fde7ec35cc9e33782
morpho-os_team <morpho-os@users.noreply.github.com>
2017-03-16 00:32:53 +0300

    Added Must::beNull()

--- lib/Morpho/Base/Must.php
+++ lib/Morpho/Base/Must.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types = 1);
+
 namespace Morpho\Base;
 
 class Must {
@@ -30,39 +32,43 @@ class Must {
         return $n == 1 ? $args[0] : $args;
     }
 
-    public static function beOneOf($needle, array $haystack)/*: void */ {
+    public static function beOneOf($needle, array $haystack): void {
         self::beTrue(in_array($needle, $haystack, true), 'The value is not one of the provided values');
     }
 
-    public static function haveOnlyKeys(array $arr, array $allowedKeys)/*: void */ {
+    public static function haveOnlyKeys(array $arr, array $allowedKeys): void {
         $diff = array_diff_key($arr, array_flip($allowedKeys));
         if (count($diff)) {
             throw new \RuntimeException('Not allowed items are present: ' . shorten(implode(', ', array_keys($diff)), 80));
         }
     }
 
-    public static function haveKeys(array $arr, array $requiredKeys)/*: void */ {
+    public static function haveKeys(array $arr, array $requiredKeys): void {
         $intersection = array_intersect_key(array_flip($requiredKeys), $arr);
         if (count($intersection) != count($requiredKeys)) {
             throw new \RuntimeException("Required items are missing");
         }
     }
 
-    public static function beInArray($value, $array)/*: void */ {
+    public static function beInArray($value, $array): void {
         self::beTrue(in_array($value, $array, true), "Value '$value' was not found in array");
     }
 
-    public static function beInRange($value, $start, $end)/*: void */ {
+    public static function beInRange($value, $start, $end): void {
         if ($value < $start || $value > $end) {
             throw new \OutOfRangeException("The value $value is out of range");
         }
     }
 
-    public static function beNotNull($value)/*: void */ {
+    public static function beNull($value): void {
+        self::beTrue($value === null);
+    }
+
+    public static function beNotNull($value): void {
         self::beTrue($value !== null);
     }
 
-    public static function beTrue($result, string $errMessage = null)/*: void */ {
+    public static function beTrue($result, string $errMessage = null): void {
         $result = (bool)$result;
         if ($result === false) {
             throw new \RuntimeException($errMessage);
