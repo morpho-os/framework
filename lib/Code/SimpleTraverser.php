62669bc30c3602575e0e4a61649ed504918f2612
morpho-os_team <morpho-os@users.noreply.github.com>
2017-05-25 09:27:44 +0300

    Fixed call of the visitor: must be called for each Node

--- lib/Code/SimpleTraverser.php
+++ lib/Code/SimpleTraverser.php
@@ -8,13 +8,13 @@ class SimpleTraverser {
     public function traverseArray(array $nodes, \Closure $fn) {
         foreach ($nodes as $node) {
             if ($node instanceof Node) {
-                $fn($node);
                 $this->traverseNode($node, $fn);
             }
         }
     }
 
     private function traverseNode(Node $node, \Closure $fn) {
+        $fn($node);
         foreach ($node->getSubNodeNames() as $name) {
             $subNode =& $node->$name;
             if (\is_array($subNode)) {
