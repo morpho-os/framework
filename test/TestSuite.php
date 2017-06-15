7a7ebb0ac26482affb1cb0088e644bc887e33392
morpho-os_team <morpho-os@users.noreply.github.com>
2017-06-15 23:29:57 +0300

    Fixed test suite

--- test/TestSuite.php
+++ test/TestSuite.php
@@ -1,35 +1,33 @@
 <?php declare(strict_types=1);
 namespace MorphoTest;
 
-use function Morpho\Base\filter;
-use const Morpho\Core\LIB_DIR_NAME;
 use const Morpho\Core\MODULE_DIR_PATH;
 use const Morpho\Core\TEST_DIR_NAME;
-use const Morpho\Core\VENDOR_DIR_NAME;
-use Morpho\Fs\Directory;
 
 class TestSuite extends \Morpho\Test\TestSuite {
     public function testFilePaths(): iterable {
-        $filePaths = Directory::filePaths(__DIR__, $this->testFileRegexp, ['recursive' => true]);
-        return array_merge(
-            filter(
-                function ($path) {
-                    return $path !== str_replace('\\', '/', __FILE__);
-                },
-                iterator_to_array($filePaths, false)
-            ),
-            $this->testFilePathsOfModules()
-        );
+        foreach ($this->testFilesInDir(__DIR__) as $file) {
+            yield $file->getPathname();
+        }
+        foreach (new \DirectoryIterator(MODULE_DIR_PATH) as $path) {
+            if ($path->isDot()) {
+                continue;
+            }
+            $testDirPath = $path->getPathname() . '/' . TEST_DIR_NAME;
+            if (is_dir($testDirPath)) {
+                foreach ($this->testFilesInDir($testDirPath) as $file) {
+                    yield $file->getPathname();
+                }
+            }
+        }
     }
 
-    protected function testFilePathsOfModules() {
-        $filter = function ($path, $isDir) {
-            if ($isDir) {
-                $baseName = basename($path);
-                return $baseName !== VENDOR_DIR_NAME && $baseName !== LIB_DIR_NAME;
-            }
-            return (bool) preg_match('~/' . TEST_DIR_NAME . '/.+Test\.php$~s', $path);
-        };
-        return iterator_to_array(Directory::filePaths(MODULE_DIR_PATH, $filter, ['recursive' => true]), false);
+    private function testFilesInDir(string $dirPath) {
+        return new \RegexIterator(
+            new \RecursiveIteratorIterator(
+                new \RecursiveDirectoryIterator($dirPath)
+            ),
+            $this->testFileRegexp
+        );
     }
 }
