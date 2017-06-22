21f3c5410f3480b410ddbb97cffe347487280e3a
morpho-os_team <morpho-os@users.noreply.github.com>
2017-06-22 04:13:48 +0300

    Don't delete the 'cache/.gitignore' file

--- module/system/controller/CacheController.php
+++ module/system/controller/CacheController.php
@@ -7,7 +7,17 @@ use Morpho\Web\Controller;
 class CacheController extends Controller {
     public function clearAllAction() {
         $cacheDirPath = $this->serviceManager->get('site')->cacheDirPath();
-        Directory::delete($cacheDirPath, false);
+        $gitIgnoreFileExists = is_file($cacheDirPath . '/.gitignore');
+        Directory::delete($cacheDirPath, function (string $path, $isDir) use ($cacheDirPath, $gitIgnoreFileExists) {
+            if ($isDir) {
+                return $path !== $cacheDirPath;
+            } else {
+                if (!$gitIgnoreFileExists) {
+                    return true;
+                }
+                return $path !== $cacheDirPath . '/.gitignore';
+            }
+        });
         $this->redirectToHome("The cache has been cleared successfully");
     }
 
