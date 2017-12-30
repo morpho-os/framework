cb7db582f8cc9a7ef06a72c5bb2fdca7718c996c
morpho-os_team <morpho-os@users.noreply.github.com>
2017-12-30 04:51:19 +0300

    #230 Added removing the declare statement

--- lib/Infra/DeclareStmtManager.php
+++ lib/Infra/DeclareStmtManager.php
@@ -7,73 +7,119 @@
 namespace Morpho\Infra;
 
 class DeclareStmtManager {
+    private $offset;
+    private $code;
+    private const OPEN_TAG_RE = '~^<\?php(?:[\040\t]|\n)~si';
+    private const MULTI_COMMENT_RE = '~/\*.*?\*/~siA';
+    private const SINGLE_COMMEN_RE = '~//[^\n\r]*~siA';
+    private const DECLARE_RE = '~declare\s*\(strict_types\s*=\s*[01]\s*\)\s*;~siA';
+
     public function removeCommentedOutDeclareStmt(string $code): string {
-        if (!preg_match('~^<\?php(?:[\040\t]|\n)~si', $code, $match)) {
+        if (!preg_match(self::OPEN_TAG_RE, $code, $match)) {
             return $code;
         }
-        $offset = strlen($match[0]);
+        $this->code = $code;
+        $this->offset = strlen($match[0]);
         $n = strlen($code);
 
-        $match = function ($re) use ($code, &$offset) {
-            preg_match($re, $code, $match, 0, $offset);
-            return $match;
-        };
-        $skip = function ($re) use ($match, &$offset) {
-            $res = $match($re);
-            if ($res) {
-                $offset += strlen($res[0]);
-            }
-        };
-        $skipSpaces = function () use ($skip) {
-            $skip('~\s+~siA');
-        };
-
-        $declareRe = '~declare\s*\(strict_types\s*=\s*[01]\s*\)\s*;~siA';
-        $multiCommentRe = '~/\*.*?\*/~siA';
-        $singleCommentRe = '~//[^\n\r]*~siA';
         $declareFound = false;
         $locs = [];
-        while ($offset < $n) {
-            $skipSpaces();
-            if ($match($multiCommentRe)) {
-                $skip($multiCommentRe);
-                $skipSpaces();
+        while ($this->offset < $n) {
+            $this->skipSpaces();
+            if ($this->match(self::MULTI_COMMENT_RE)) {
+                $this->skip(self::MULTI_COMMENT_RE);
+                $this->skipSpaces();
                 continue;
             }
-            if ($match($singleCommentRe)) {
-                $res = $match('~//\s*declare\s*\(strict[^\n\r]*~siA');
+            if ($this->match(self::SINGLE_COMMEN_RE)) {
+                $res = $this->match('~//\s*declare\s*\(strict[^\n\r]*~siA');
                 if ($res) {
-                    $locs[] = [$offset, strlen($res[0])];
+                    $locs[] = [$this->offset, strlen($res[0])];
                 }
-                $skip($singleCommentRe);
-                $skipSpaces();
+                $this->skip(self::SINGLE_COMMEN_RE);
+                $this->skipSpaces();
                 continue;
             }
-            if (!$declareFound && $match($declareRe)) {
-                $skip($declareRe);
-                $skipSpaces();
+            if (!$declareFound && $this->match(self::DECLARE_RE)) {
+                $this->skip(self::DECLARE_RE);
+                $this->skipSpaces();
                 $declareFound = true;
                 continue;
             }
-            if ($match('~\S~siA')) {
+            if ($this->match('~\S~siA')) {
                 break;
             }
         }
         if ($locs) {
-            $newCode = '';
-            $p = 0;
-            foreach ($locs as $loc) {
-                [$offset, $length] = $loc;
-                $newCode .= substr($code, $p, $offset);
-                if (preg_match('~^<\?php\s+$~si', $newCode)) {
-                    $newCode = rtrim($newCode) .  substr($code, $offset + $length);
-                } else {
-                    $newCode .= ltrim(substr($code, $offset + $length));
-                }
-                $p += $length;
-            }
-            return $newCode;
+            return $this->removeLocs($code, $locs);
         }
         return $code;
+    }
+
+    public function removeDeclareStmt(string $code): string {
+        if (!preg_match(self::OPEN_TAG_RE, $code, $match)) {
+            return $code;
+        }
+        $this->code = $code;
+        $this->offset = strlen($match[0]);
+        $n = strlen($code);
+
+        $locs = [];
+        while ($this->offset < $n) {
+            $this->skipSpaces();
+            if ($this->match(self::MULTI_COMMENT_RE)) {
+                $this->skip(self::MULTI_COMMENT_RE);
+                $this->skipSpaces();
+                continue;
+            }
+            if ($this->match(self::SINGLE_COMMEN_RE)) {
+                $this->skip(self::SINGLE_COMMEN_RE);
+                $this->skipSpaces();
+                continue;
+            }
+            if ($res = $this->match(self::DECLARE_RE)) {
+                $locs[] = [$this->offset, strlen($res[0])];
+                break;
+            }
+            if ($this->match('~\S~siA')) {
+                break;
+            }
+        }
+        if ($locs) {
+            return $this->removeLocs($code, $locs);
+        }
+        return $code;
+    }
+
+    private function skipSpaces(): void {
+        $this->skip('~\s+~siA');
+    }
+
+    private function skip(string $re): void {
+        $res = $this->match($re);
+        if ($res) {
+            $this->offset += strlen($res[0]);
+        }
+    }
+
+    private function match(string $re): array {
+        preg_match($re, $this->code, $match, 0, $this->offset);
+        return $match;
+    }
+
+    private function removeLocs(string $code, array $locs): string {
+        $newCode = '';
+        $p = 0;
+        foreach ($locs as $loc) {
+            [$offset, $length] = $loc;
+            $newCode .= substr($code, $p, $offset);
+            if (preg_match('~^<\?php\s+$~si', $newCode)) {
+                $newCode = rtrim($newCode) .  substr($code, $offset + $length);
+            } else {
+                $newCode .= ltrim(substr($code, $offset + $length));
+            }
+            $p += $length;
+        }
+        return rtrim($newCode);
     }
 }
--- test/unit/Infra/DeclareStmtManagerTest.php
+++ test/unit/Infra/DeclareStmtManagerTest.php
@@ -46,12 +46,77 @@ OUT
             , $expected
         ];
 
-        yield [<<<OUT
+        yield [
+            <<<OUT
 <?php
 //declare(strict_types=1);
 OUT
             , '<?php'
         ];
+        yield from $this->commonCases();
+    }
+
+    /**
+     * @dataProvider dataForRemoveCommentedOutDeclareStmt
+     */
+    public function testRemoveCommentedOutDeclareStmt(string $code, string $expected) {
+        $manager = new DeclareStmtManager();
+        $this->assertSame($expected, $manager->removeCommentedOutDeclareStmt($code));
+    }
+
+    public function dataForRemoveDeclareStmt() {
+        $expected = <<<OUT
+<?php
+/**
+ * This file is part of morpho-os/framework
+ */
+echo "Test";
+OUT;
+        yield [
+            <<<OUT
+<?php declare(strict_types=1);
+/**
+ * This file is part of morpho-os/framework
+ */
+echo "Test";
+OUT
+            , $expected
+        ];
+        yield [
+            <<<OUT
+<?php
+//declare(strict_types=1);
+OUT
+            ,
+            <<<OUT
+<?php
+//declare(strict_types=1);
+OUT
+        ];
+        yield [
+            <<<OUT
+<?php
+//declare(strict_types=1);
+declare(strict_types=1);
+OUT
+            ,
+            <<<OUT
+<?php
+//declare(strict_types=1);
+OUT
+        ];
+        yield from $this->commonCases();
+    }
+
+    /**
+     * @dataProvider dataForRemoveDeclareStmt
+     */
+    public function testRemoveDeclareStmt(string $code, string $expected) {
+        $manager = new DeclareStmtManager();
+        $this->assertSame($expected, $manager->removeDeclareStmt($code));
+    }
+
+    private function commonCases(): iterable {
         yield [
             <<<OUT
 <?php
@@ -79,13 +144,5 @@ OUT
             '',
             '',
         ];
-    }
-
-    /**
-     * @dataProvider dataForRemoveCommentedOutDeclareStmt
-     */
-    public function testRemoveCommentedOutDeclareStmt(string $code, string $expected) {
-        $manager = new DeclareStmtManager();
-        $this->assertSame($expected, $manager->removeCommentedOutDeclareStmt($code));
     }
 }
