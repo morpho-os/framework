a349f5a34c58112d939980db4df8753a8d02e1fb
morpho-os_team <morpho-os@users.noreply.github.com>
2018-01-01 15:12:58 +0300

    #230 Added the declare statement for php files of modules

--- module/system/lib/Web/IndexController.php
+++ module/system/lib/Web/IndexController.php
@@ -1,4 +1,4 @@
-<?php //declare(strict_types=1);
+<?php declare(strict_types=1);
 namespace Morpho\System\Web;
 
 use Morpho\Web\Controller;
--- module/user/lib/Domain/UserRepo.php
+++ module/user/lib/Domain/UserRepo.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 namespace Morpho\User\Domain;
 
 use Morpho\Base\ArrayTool;
