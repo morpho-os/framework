c44cb43b6e6308803ed3e237d069bc44b245cc72
morpho-os_team <morpho-os@users.noreply.github.com>
2018-01-26 18:36:25 +0300

    #264 Added G11n namespace

---
+++ lib/G11n/Locale.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of morpho-os/framework
+ * It is distributed under the 'Apache License Version 2.0' license.
+ * See the https://github.com/morpho-os/framework/blob/master/LICENSE for the full license text.
+ */
+namespace Morpho\G11n;
+
+class Locale {
+/*
+From locale(5):
+       *  LC_CTYPE
+
+       *  LC_COLLATE
+
+       *  LC_MESSAGES
+
+       *  LC_MONETARY
+
+       *  LC_NUMERIC
+
+       *  LC_TIME
+
+       In addition, since version 2.2, the GNU C library supports the following nonstandard categories:
+
+       *  LC_ADDRESS
+
+       *  LC_IDENTIFICATION
+
+       *  LC_MEASUREMENT
+
+       *  LC_NAME
+
+       *  LC_PAPER
+
+       *  LC_TELEPHONE
+
+ */
+}
