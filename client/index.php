93ba5669afe2af7fdfebcd4c0903d51f4d16eb79
morpho-os_team <morpho-os@users.noreply.github.com>
2020-06-14 23:39:27 +0300

    #413 Setting up CI

--- client/index.php
+++ client/index.php
@@ -1,3 +1,4 @@
 <?php declare(strict_types=1);
+die('ok');
 require __DIR__ . '/../vendor/autoload.php';
 \Morpho\App\App::main(new \ArrayObject(require __DIR__ . '/../server/localhost/config/app.config.php'));
