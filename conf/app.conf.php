9a87a47e52a22e4fe65adca5fee84ef7092585c3
morpho-os_team <morpho-os@users.noreply.github.com>
2021-05-14 06:41:12 +0300

    Issue #275: WIP

--- conf/app.conf.php
+++ conf/app.conf.php
@@ -1,21 +1,25 @@
 <?php declare(strict_types=1);
-use Morpho\App;
-use const Morpho\App\{CONF_DIR_NAME, BACKEND_DIR_NAME, VENDOR, SITE_CONF_FILE_NAME};
+/**
+ * This file is part of morpho-os/framework
+ * It is distributed under the 'Apache License Version 2.0' license.
+ * See the https://github.com/morpho-os/framework/blob/master/LICENSE for the full license text.
+ */
+namespace Morpho\App;
 
 $baseDirPath = realpath(__DIR__ . '/..');
 return [
-    'siteFactory' => function (App\App $app) {
-        $hostNameValidator = PHP_SAPI === 'cli'
-            ? new App\Cli\HostNameValidator(['localhost'], 'localhost')
-            : new App\Web\HostNameValidator(['framework', 'localhost', '127.0.0.1']);
-        return new App\SiteFactory($hostNameValidator, $app->conf());
+    'siteFactory' => function (App $app) {
+        $hostNameValidator = PHP_SAPI === 'cli' ? new Cli\HostNameValidator(['localhost'], 'localhost') : new Web\HostNameValidator(
+            ['framework', 'localhost', '127.0.0.1']
+        );
+        return new SiteFactory($hostNameValidator, $app->conf());
     },
-    'sites' => [
+    'sites'       => [
         'localhost' => [
             'module' => [
-                'name' => VENDOR . '/localhost',
+                'name'  => VENDOR . '/localhost',
                 'paths' => [
-                    'dirPath' => $baseDirPath . '/' . BACKEND_DIR_NAME . '/localhost',
+                    'dirPath'      => $baseDirPath . '/' . BACKEND_DIR_NAME . '/localhost',
                     'confFilePath' => $baseDirPath . '/' . BACKEND_DIR_NAME . '/localhost/' . CONF_DIR_NAME . '/' . SITE_CONF_FILE_NAME,
                 ],
             ],
